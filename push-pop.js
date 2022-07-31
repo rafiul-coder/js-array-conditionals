c0c883bdfefeec97c371485437673c3846b4644a
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-01 00:39:14 +0600

    array push

---
+++ push-pop.js
@@ -0,0 +1,10 @@
+var numbers = [78, 79, 82, 90];
+
+//use push to add elements to an array as the last elements array
+
+numbers.push(100);
+// console.log(numbers);
+
+var friends = ['rohim', 'karim', 'jabbar'];
+friends.push('jabbar');
+console.log(friends);
