04f9454db52c1ae952a71dbc53594b1113465731
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-01 00:30:14 +0600

    get set and find index

---
+++ index.js
@@ -0,0 +1,23 @@
+var numbers = [45, 54, 65, 74, 87];
+
+// get element value by index
+var element = numbers[1];
+// console.log(numbers);
+// console.log(numbers[4]);
+//get element value by index
+var element = numbers[2];
+// console.log(element)
+
+//set element value by index
+
+numbers[3] = 99;
+numbers[3] = 55;
+// console.log(numbers);
+
+//find index of an element
+
+var positionIndex = numbers.indexOf (87);
+console.log(positionIndex);
+
+var positionIndex = numbers.indexOf (867);// result asbe -1
+console.log(positionIndex);
