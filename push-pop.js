61e2a62de49fe78b82f4638704b46cd6b7565031
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-01 00:58:42 +0600

    add elements to begginers

--- push-pop.js
+++ push-pop.js
@@ -2,9 +2,17 @@ var numbers = [78, 79, 82, 90];
 
 //use push to add elements to an array as the last elements array
 
-numbers.push(100);
+// numbers.push(100);
 // console.log(numbers);
 
 var friends = ['rohim', 'karim', 'jabbar'];
 friends.push('jabbar');
-console.log(friends);
+// console.log(friends);
+
+//use pop to get read of the last elements
+// numbers.pop();
+// console.log(numbers);
+
+//use push to add elements to an array as the begginers elements array
+numbers.shift();
+console.log(numbers);
