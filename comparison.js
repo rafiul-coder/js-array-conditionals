83e1de0a3a9de26b6688c78778ff23a380fd412b
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-01 18:53:21 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,13 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(5 != 6);
+// console.log(15 != 15);
+// console.log(5 <= 6);
+// console.log(6 >= 6);
+// console.log(16 < 6);
+
+//getter than or equal
+
+console.log(5 >= 6);
