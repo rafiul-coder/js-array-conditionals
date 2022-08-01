339db567bc2c8b7455fe1be91a06bb6f35bf73c2
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-01 23:10:17 +0600

    conditional js

---
+++ conditional.js
@@ -0,0 +1,39 @@
+var iphonePrice = 70000;
+var myBudget = 90000;
+
+// if iphone price is less than my budget.i will buy the phone
+
+/**
+ * if iphone price is less than my budget
+ * i will buy the phone
+ * 
+ * if(condition){
+ *         //will execute if the condition is true
+ * }
+ * 
+ */
+// if (iphonePrice < myBudget){
+//     console.log('i phone kine futani marbo')
+
+
+var chickenPrice = 500;
+var myMoney = 50;
+// if(chickenPrice < myMoney){
+//     console.log('I will eat chicken')
+// }
+// if(chickenPrice > myMoney){
+//     console.log('Singara alu khabo')
+// }
+
+
+/**
+ * if chicken price is less than my budget
+ * i will eat chicken
+ * other wise else 
+ */
+if(chickenPrice < myMoney){
+    console.log('I will eat chicken')
+}
+else{
+    console.log('I will eat potato')
+}
