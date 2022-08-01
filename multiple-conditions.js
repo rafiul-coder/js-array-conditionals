267253a98f2024626a3ef1071224cdc481f9cacb
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-01 23:33:20 +0600

    multiple coonditions

---
+++ multiple-conditions.js
@@ -0,0 +1,33 @@
+// var isGraduated = true;
+// var sallary = 75000;
+// var car = 1;
+// if (isGraduated == true) {
+//     console.log('I am success')
+// }
+// else {
+//     console.log('Tor Kopale biye nai')
+// }
+// if (isGraduated == true && sallary > 50000){
+//     console.log('Easho biye kore feli')
+// }
+// else {
+//     console.log('Tor Kopale biye nai')
+// }
+
+// if (isGraduated == true && sallary > 50000 && car >= 1){
+//     console.log('Easho biye kore feli')
+// }
+// else {
+//     console.log('Tor Kopale biye nai')
+// }
+
+var isGraduated = false;
+var sallary = 7000;
+var car = 0;
+
+if (isGraduated == true || sallary >50000 || car >= 1){
+    console.log('Eaasho Prem kori')
+}
+else{
+    console.log('Tomar Friend Er number deo')
+}
