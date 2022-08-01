32f68b554f94b5695603d6116ea6f2893665c237
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-01 23:50:33 +0600

    multi-stage-conditions

---
+++ multi-stage-conditions.js
@@ -0,0 +1,17 @@
+var money = 10;
+var danishPrice = 45;
+var biscuitPrice = 30;
+var butterPrice = 20;
+
+if (danishPrice < money){
+    console.log('Danish khabo')
+}
+else if(biscuitPrice < money){
+    console.log('Ha ha biscuit khabo')
+}
+else if(butterPrice < money){
+    console.log('Yeammee Butter')
+}
+else{
+    console.log('Khali caa e shasther jonno valo')
+}
