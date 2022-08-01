915abb40c365148681bd8abddf6f8ece1b0263de
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-02 00:30:46 +0600

    problem solved

---
+++ problem-solved.js
@@ -0,0 +1,75 @@
+var totalMarks = 85;
+
+var JanesMarks = 95;
+var tomMarks = 66;
+var piterMarks = 56;
+var johnsMarks = 40;
+
+//janes marks
+if (JanesMarks >= 80) {
+    console.log('A Grade')
+}
+else if (JanesMarks >= 60) {
+    console.log('B grage')
+}
+else if (JanesMarks >= 50) {
+    console.log('C Grade')
+}
+else if (JanesMarks >= 40) {
+    console.log('D Grade')
+}
+else {
+    console.log('F grade')
+}
+
+//toms markes
+
+if (tomMarks >= 80) {
+    console.log('A Grade')
+}
+else if (tomMarks >= 60) {
+    console.log('B grage')
+}
+else if (tomMarks >= 50) {
+    console.log('C Grade')
+}
+else if (tomMarks >= 40) {
+    console.log('D Grade')
+}
+else {
+    console.log('F grade')
+}
+//piter marks
+
+if (piterMarks >= 80) {
+    console.log('A Grade')
+}
+else if (piterMarks >= 60) {
+    console.log('B grage')
+}
+else if (piterMarks >= 50) {
+    console.log('C Grade')
+}
+else if (piterMarks >= 40) {
+    console.log('D Grade')
+}
+else {
+    console.log('F grade')
+}
+//johns markes
+
+if (johnsMarks >= 80) {
+    console.log('A Grade')
+}
+else if (johnsMarks >= 60) {
+    console.log('B grage')
+}
+else if (johnsMarks >= 50) {
+    console.log('C Grade')
+}
+else if (johnsMarks >= 40) {
+    console.log('D Grade')
+}
+else {
+    console.log('F grade')
+}
